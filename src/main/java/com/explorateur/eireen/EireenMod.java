d74b96d309df72e217c0dbe5837e30ed05feb04d
Josia <elwyn.vdb@gmail.com>
2021-04-08 16:37:29 +0200

    Mutanic

--- src/main/java/com/explorateur/eireen/EireenMod.java
+++ src/main/java/com/explorateur/eireen/EireenMod.java
@@ -3,15 +3,13 @@ package com.explorateur.eireen;
 import com.explorateur.eireen.client.init.EntityRenderRegistry;
 import com.explorateur.eireen.common.init.EEntities;
 import com.explorateur.eireen.registry.EireenRegistry;
-import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
 import net.minecraft.entity.monster.ZombieEntity;
-import net.minecraft.entity.passive.CatEntity;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -34,7 +32,6 @@ public class EireenMod {
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        GlobalEntityTypeAttributes.put(EEntities.MUTANIC.get(), ZombieEntity.createAttributes().build());
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
@@ -43,6 +40,7 @@ public class EireenMod {
 
 
     @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
+    public void onServerStarting(EntityAttributeCreationEvent event) {
+        event.put(EEntities.MUTANIC.get(), ZombieEntity.createAttributes().build());
     }
 }
